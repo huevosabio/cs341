a5d8472b6f965f057fef2d090a965f099dc7df57
Federico Rossi <federico.rossi.314@gmail.com>
2017-06-01 17:50:07 -0700

    Towards MATLAB sim

---
+++ BuildCityMap.m
@@ -0,0 +1,25 @@
+LinkTime=csvread('travel_time.csv',3,1);
+NodesLocation=csvread('inferred_locations.csv',1,1);
+N=length(NodesLocation);
+
+StationLocation=NodesLocation;
+StationNodeID=[1:1:N]';
+
+
+RoadGraph=cell(N,1);
+LinkFreeFlow=zeros(N,N);
+LinkLength=LinkFreeFlow;
+RoadCap=LinkFreeFlow;
+MAXCAP=1e6;
+
+for i=1:N
+    for j=1:N
+        if LinkTime(i,j)>0
+            RoadGraph{i}=[RoadGraph{i},j];
+            LinkLength(i,j)=norm(NodesLocation(j,:) - NodesLocation(i,:));
+            LinkFreeFlow(i,j)=LinkLength(i,j)/LinkTime(i,j);
+            RoadCap=MAXCAP;
+        end
+    end
+end
+
